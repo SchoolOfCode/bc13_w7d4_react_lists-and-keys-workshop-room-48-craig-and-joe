b009e3ff86bfec6aca665b63e581cb92359e5705
[Creg Grady] <112422397+CregCodes@users.noreply.github.com>
2022-11-10 18:25:04 +0000

    rendered elements and properties

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -9,7 +9,43 @@ import Post from '../Post';
 function Main() {
   const [posts, setPosts] = useState(samplePosts);
 
-  return <main id="main"></main>;
-}
+  return <main id="main">
+ 
+  {posts.map(({ postId, title, date, author, text, highlights, image }) => {
+    return <Post key={postId} title={title} date={date} author={author} text={text} highlights={highlights} image={image} />
+  } )}
+  </main>; 
+  }
+
+  // we want this <Post/>
+    // We a data structure
+      // for how many posts with this structure- the map method creates enough <Post />
+        //create map method function ()
+        // wrap it around a <Post /> Component 
+
 
 export default Main;
+
+// {
+//   postId: 1,
+//   title: 'One Of My Favourites',
+//   date: '12/10/20',
+//   author: 'Bobby Nest',
+//   text: lorem.generateParagraphs(7),
+//   highlights: [
+//     lorem.generateWords(3),
+//     lorem.generateWords(3),
+//     lorem.generateWords(3),
+//   ],
+//   image: {
+//     link:
+//       'https://i.pinimg.com/originals/71/a0/5f/71a05f47b8f449fc231881416bf4bc66.jpg',
+//     alt: 'A blue-grey pigeon turns to face the camera.',
+//   },
+// }
+
+// {posts.map(({postId, title, date, author, text, highlights, link, alt}) => {
+//   return <main id="main">
+//     <Post key={postId}, title={title}, date={date}, author={author}, text={text}, highglights={highlights}, url={link}, alt={alt} /> 
+//   </main>; 
+//   }}
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 import './index.css';
 
-function Post() {
-  return <article></article>;
+function Post({title, date, author, text, highlights, image }) {
+  return <article> <img src={image.link} alt={image.alt} width={'80%'}  /> <h1>{title}</h1> <h3>{author}</h3><time>{date}</time> <p>{text}</p> {highlights.map((highlight, index) => {
+    return <li>{highlight}{index}</li>
+  })} </article>;
 }
+
+//not including image or image properties
+// <img src=(image.link) alt=(image.alt) />
+
+//<Post key={postId} title={title} date={date} author={author} text={text} highlights={highlights} link={link} alt={alt}/>
+
 
 export default Post;
